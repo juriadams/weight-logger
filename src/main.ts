109495a14c39e4571f5e50a4ee090cdef9f3a701
Juri Adams <juri@adams.sh>
2022-01-14 14:45:42 +0100

    🚀 Bootstrapped new Nest App

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import * as env from "dotenv";
+
+import { NestFactory } from "@nestjs/core";
+
+import { AppModule } from "./app.module";
+
+env.config();
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+}
+bootstrap();
